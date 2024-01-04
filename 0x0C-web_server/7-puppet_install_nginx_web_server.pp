53e0a389d114553f5d17196bf53620779404eeb1
MihdarAbbasher <mihdar.ph@gmail.com>
2024-01-04 22:48:40 +0200

    t7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+# Puppet: to install nginx - auto
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'abcd':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=AZg4uJkEa-4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
