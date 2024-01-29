298c74332718ebd75191ab761283bba8283b4128
MihdarAbbasher <mihdar.ph@gmail.com>
2024-01-29 05:12:03 -0800

    t3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Accessing a REST API for employees todo lists"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/" + employeeId
+    response = requests.get(url)
+    userName = response.json().get('username')
+    url += "/todos"
+    response = requests.get(url)
+    allTasks = response.json()
+    mydic[employeeId] = []
+    for task in allTasks:
+        mydic[employeeId].append({
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": userName
+        })
+    with open('{}.json'.format(employeeId), 'w') as myfile:
+        json.dump(mydic, myfile)
