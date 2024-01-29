85c14e512c0ba93a70f96c8f35cd39c6964a4322
MihdarAbbasher <mihdar.ph@gmail.com>
2024-01-29 04:20:21 -0800

    t1up

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -17,13 +17,6 @@ if __name__ == '__main__':
     doneTasks = []
     with open('{}.csv'.format(employeeId), 'w') as myfile:
         for task in allTasks:
-            line = '"{},"{}","{}","{}"\n'.format(employeeId, emplyeeName,
-                task.get('completed'), task.get('title')
+            line = '"{},"{}","{}","{}"\n'.format(employeeId, employeeName,
+                task.get('completed'), task.get('title'))
             myfile.write(line)
-        if task.get('completed'):
-            doneTasks.append(task)
-            done += 1
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employeeName, done, len(allTasks)))
-    for task in doneTasks:
-        print("\t {}".format(task.get('title')))
